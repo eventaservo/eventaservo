e3670520ff38d236f165eae9aa7b13b3ca9e64cb
Fernando Shayani <shayani@gmail.com>
2018-10-16 09:28:30 -0300

    Devise

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :timeoutable, :trackable,
+         :omniauthable, omniauth_providers: %i[facebook]
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  root to: 'home#index'
 end
