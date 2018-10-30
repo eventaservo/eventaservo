0589fb978ee135ddda693f0bd95497c1916f10d4
Fernando Shayani <shayani@gmail.com>
2018-10-30 10:00:52 -0300

    Devise tradukoj

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'eventaservo.org', port: 443 }
+  config.action_mailer.default_url_options = { host: 'eventaservo.org' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
