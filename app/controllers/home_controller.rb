1e31cbeba9cb90b0da60b53b81d86b4f3d3e7716
Fernando Shayani <shayani@gmail.com>
2018-10-29 22:12:38 -0300

    Detaloj

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @events = Event.venontaj.grouped_by_months
+    @events = Event.includes(:country).joins(:likes).joins(:participants).venontaj.grouped_by_months
     @countries = Event.venontaj.count_by_country
   end
 
   def privateco
-
   end
 end
