4a4841ac399770ce69ccde16349daa40bbd29a57
Fernando Ŝajani <shayani@gmail.com>
2019-03-13 07:50:12 -0300

    Provkodoj por #70

--- test/integration/post_event_test.rb
+++ test/integration/post_event_test.rb
@@ -12,7 +12,7 @@ class PostEventTest < ActionDispatch::IntegrationTest
 
   test 'kreas novan eventon' do
     sign_in create(:uzanto, :admin)
-    lando = create(:lando, :brazilo)
+    brazilo = create(:lando, :brazilo)
 
     get '/eventoj/new'
     assert_response :success
@@ -23,8 +23,8 @@ class PostEventTest < ActionDispatch::IntegrationTest
              params: {
                event: {
                  title: Faker::Book.title, description: Faker::Lorem.sentence(3), content: Faker::Lorem.paragraph(3),
-                 city: 'Joan-Pesoo', country_id: lando.id, site: Faker::Internet.url,
-                 date_start: '01/01/2019', date_end: '01/01/2019'
+                 city: 'Ĵoan-Pesoo', country_id: brazilo.id, site: Faker::Internet.url,
+                 date_start: '17/07/2019', date_end: '17/07/2019'
                },
                time_start: '14:00', time_end: '16:00'
              }
@@ -34,5 +34,13 @@ class PostEventTest < ActionDispatch::IntegrationTest
         assert_select 'div.flash-alert-box', /Evento sukcese kreita/
       end
     end
+
+    evento = Event.last
+    assert_equal 'Ĵoan-Pesoo', evento.city
+    assert_equal 'America/Fortaleza', evento.time_zone
+    assert_equal '17/07/2019', evento.komenca_tago
+    assert_equal '14:00', evento.komenca_horo
+    assert_equal '17/07/2019', evento.fina_tago
+    assert_equal '16:00', evento.fina_horo
   end
 end
