cb099dda8b8d51d2a28a3a39f6728c00e0a6c3c1
Fernando Ŝajani <shayani@gmail.com>
2019-03-14 18:52:15 -0300

    Agordas Raven

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::Base
   private
 
     def set_raven_context
-      Raven.user_context(id: session[:current_user_id]) # or anything else in session
+      Raven.user_context(id: session[:current_user_id]) if user_signed_in?
       Raven.extra_context(params: params.to_unsafe_h, url: request.url)
     end
 
