177ad6b496ad82aa4ac4299c271960a883a37cd1
Fernando Ŝajani <shayani@gmail.com>
2019-05-03 14:56:19 -0300

    Dev-dosieroj

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   config.force_ssl = true
 
   config.after_initialize do
-    Bullet.enable              = true
+    Bullet.enable              = false
     Bullet.bullet_logger       = true
     Bullet.console             = false
     Bullet.rails_logger        = false
