be71e06f521edac292c121820cc27b4fa4b3e8e7
Fernando Ŝajani <shayani@gmail.com>
2019-05-10 17:14:29 -0300

    Rack attack agordoj kaj sciiga informo

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -3,7 +3,7 @@ class Rack::Attack
     '127.0.0.1' == req.ip || '::1' == req.ip
   end
 
-  throttle('requets by IP', limit: 30, period: 5) do |req|
+  throttle('requets by IP', limit: 25, period: 5) do |req|
     req.ip
   end
 
@@ -14,3 +14,15 @@ class Rack::Attack
   end
 end
 
+class NotificationsMailer < ApplicationMailer
+  def rack_mailer(payload)
+    mail(to: 'kontakto@eventaservo.org', subject: 'Rack attack log info') do
+      render plain: payload
+    end
+  end
+end
+
+ActiveSupport::Notifications.subscribe(/rack_attack/) do |name, start, finish, request_id, payload|
+  Rails.logger.debug "RACK_ATTACK: #{payload}"
+  NotificationMailer.rack_mailer(payload).deliver_later
+end
