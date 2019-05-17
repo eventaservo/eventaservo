9f21c022d2c24f22ea4d08d0292b805f00f2bba5
Fernando Ŝajani <shayani@gmail.com>
2019-05-17 15:18:50 -0300

    Riparas #103

--- app/models/event.rb
+++ app/models/event.rb
@@ -193,11 +193,9 @@ class Event < ApplicationRecord
         self.time_zone = Timezone.lookup(latitude, longitude).name
       end
 
-      if date_start_changed? || date_end_changed?
-        tz = TZInfo::Timezone.get(time_zone)
-        self.date_start = tz.local_to_utc(Time.new(date_start.year, date_start.month, date_start.day, date_start.hour, date_start.min)).in_time_zone(time_zone)
-        self.date_end = tz.local_to_utc(Time.new(date_end.year, date_end.month, date_end.day, date_end.hour, date_end.min)).in_time_zone(time_zone)
-      end
+      tz = TZInfo::Timezone.get(self.time_zone)
+      self.date_start = tz.local_to_utc(Time.new(date_start.year, date_start.month, date_start.day, date_start.hour, date_start.min)).in_time_zone(time_zone)
+      self.date_end = tz.local_to_utc(Time.new(date_end.year, date_end.month, date_end.day, date_end.hour, date_end.min)).in_time_zone(time_zone)
     end
 
     def fix_title(title)
