b552583d9d6662e489b29b461c76c9f11862b8dd
Fernando Ŝajani <shayani@gmail.com>
2019-06-25 07:30:35 -0300

    No analizas kelkajn IP-ojn

--- app/modules/constants.rb
+++ app/modules/constants.rb
@@ -6,4 +6,6 @@ module Constants
   ADMIN_EMAILS = %w[yves.nevelsteen@gmail.com].freeze
 
   TAGS = %w[Kongreso Kunveno Kurso Varbado Alia].freeze
+
+  IGNORENDAJ_IP = %w[127.0.0.1 207.180.227.223 2a02:c207:2022:6445::1].freeze
 end
