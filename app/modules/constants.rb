cc1fff95be93cc4c3b0e484e08fde64db1f71488
Fernando Ŝajani <shayani@gmail.com>
2019-08-24 10:53:39 -0300

    Plibonigoj #150 #151 #152

--- app/modules/constants.rb
+++ app/modules/constants.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 module Constants
-  VERSIO = '2.1.13 (beta)'
+  VERSIO = '2.1.14 (beta)'
+  public_constant :VERSIO
 
   ADMIN_EMAILS = %w[yves.nevelsteen@gmail.com].freeze
+  public_constant :ADMIN_EMAILS
 
   TAGS = %w[Kongreso Kunveno Kurso Varbado Alia].freeze
+  public_constant :TAGS
 
   IGNORENDAJ_IP = %w[127.0.0.1 207.180.227.223 2a02:c207:2022:6445::1].freeze
+  public_constant :IGNORENDAJ_IP
 end
