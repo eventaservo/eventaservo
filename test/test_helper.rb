02dec9c1b1af447051c14e829c115336f3e2dc08
Alejandro Sánchez Medina <alejandrosanchzmedina@gmail.com>
2019-08-31 09:34:08 +0100

    Aldonas provkodo por importilo (#155)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -61,5 +61,6 @@ class ActiveSupport::TestCase
   ::Timezone::Lookup.lookup.stub(40.71, -74.00, 'America/New_York')
   ::Timezone::Lookup.lookup.stub(-23.55, -46.63, 'America/Sao_Paulo')
   ::Timezone::Lookup.lookup.stub(-7.11, -34.86, 'America/Fortaleza')
+  ::Timezone::Lookup.lookup.stub(43.66590881347656, -79.38521575927734, 'America/Toronto')
   ::Timezone::Lookup.lookup.default('Etc/UTC')
 end
