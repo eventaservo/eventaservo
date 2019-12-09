42a050863f66d500c29730ea45fe453770b86298
Fernando Ŝajani <shayani@gmail.com>
2019-12-09 05:27:45 -0300

    Versio-numero

--- app/modules/constants.rb
+++ app/modules/constants.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Constants
-  VERSIO = '2.3.8 (beta)'
+  VERSIO = '2.3.9 (beta)'
   public_constant :VERSIO
 
   ADMIN_EMAILS = %w[yves.nevelsteen@gmail.com].freeze
