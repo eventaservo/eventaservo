b5b650094b61ba0eb91007c86423637705dc0c2f
Fernando Ŝajani <shayani@gmail.com>
2019-12-18 17:48:07 -0300

    Malŝaltu de CSP

--- config/initializers/csp.rb
+++ config/initializers/csp.rb
@@ -1,13 +1,13 @@
-unless Rails.env.development?
-  Rails.application.config.content_security_policy_report_only = true
-
-  Rails.application.config.content_security_policy do |policy|
-    policy.default_src :self, :https
-    policy.font_src    :self, :https, :data
-    policy.img_src     :self, :https, :data
-    policy.object_src  :none
-    policy.script_src  :self, :https
-    policy.style_src   :self, :https, :unsafe_inline
-    policy.report_uri  "https://eventaservo.report-uri.com/r/d/csp/reportOnly"
-  end
-end
+#unless Rails.env.development?
+#  Rails.application.config.content_security_policy_report_only = true
+#
+#  Rails.application.config.content_security_policy do |policy|
+#    policy.default_src :self, :https
+#    policy.font_src    :self, :https, :data
+#    policy.img_src     :self, :https, :data
+#    policy.object_src  :none
+#    policy.script_src  :self, :https
+#    policy.style_src   :self, :https, :unsafe_inline
+#    policy.report_uri  "https://eventaservo.report-uri.com/r/d/csp/reportOnly"
+#  end
+#end
