01533b04c57f743cd78e36224eebccc97b3125fe
Fernando Ŝajani <shayani@gmail.com>
2020-01-21 07:52:36 -0300

    Riparas telegram-mesaĝon kun specialaj signoj #213

--- app/jobs/evento_ghisdatigita_job.rb
+++ app/jobs/evento_ghisdatigita_job.rb
@@ -7,6 +7,6 @@ class EventoGhisdatigitaJob < ApplicationJob
     mesagho = "Evento ĝisdatigita:\n\n"
     mesagho += "<b>#{evento.title}</b>\n\n"
     mesagho += event_url(evento.ligilo) + '/kronologio'
-    system "telegram-send --config config/es_admin_channel.conf --format html --disable-web-page-preview \"#{mesagho}\""
+    system "telegram-send --config config/es_admin_channel.conf --format html --disable-web-page-preview \"#{I18n.transliterate(mesagho)}\""
   end
 end
