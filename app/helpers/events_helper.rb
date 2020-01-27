f4b0dec378bfa4d6334cf63d10d6f65f3ada6b75
Fernando Ŝajani <shayani@gmail.com>
2020-01-27 07:20:58 -0300

    Ŝanĝas la Specojn

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -69,4 +69,14 @@ module EventsHelper
       yield
     end
   end
+
+  def speconomo_plurale(tag)
+    case tag
+    when 'Internacia' then 'Internaciaj eventoj'
+    when 'Loka' then 'Lokaj kunvenoj'
+    when 'Kurso' then 'Kursoj'
+    when 'Alia' then 'Aliaj'
+    else tag
+    end
+  end
 end
--- app/modules/constants.rb
+++ app/modules/constants.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module Constants
-  VERSIO = '2.5.0 (beta)'
+  VERSIO = '2.6.0 (beta)'
   public_constant :VERSIO
 
   ADMIN_EMAILS = %w[yves.nevelsteen@gmail.com].freeze
   public_constant :ADMIN_EMAILS
 
-  TAGS = %w[Kongreso Kunveno Kurso Varbado Alia].freeze
+  TAGS = %w[Internacia Loka Kurso Alia].freeze
   public_constant :TAGS
 
   IGNORENDAJ_IP = %w[127.0.0.1 207.180.227.223 2a02:c207:2022:6445::1].freeze
