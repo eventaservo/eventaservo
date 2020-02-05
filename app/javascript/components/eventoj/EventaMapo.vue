de2a03a3888094c399aba2a6f8bec770e563e7a2
Fernando Ŝajani <shayani@gmail.com>
2020-02-05 05:35:39 -0300

    Ŝanĝasl la mapetan servilon de OSM al Mapbox

--- app/javascript/components/eventoj/EventaMapo.vue
+++ app/javascript/components/eventoj/EventaMapo.vue
@@ -13,8 +13,10 @@ export default {
   },
   mounted () {
     const map = L.map("map").setView([this.latitude, this.longitude], 13)
-    L.tileLayer("https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png?lang=eo", {
-      attribution: '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a>'
+    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXZlbnRhc2Vydm8iLCJhIjoiY2s2OGcxaWU5MDRtYzNucWZqdXRicnFpMyJ9.HRdmn4ful40N4svL9ix8vA", {
+      attribution: '<a href="http://www.mapbox.com">Mapbox</a>',
+      id: "mapbox/streets-v11",
+      access_toke: "pk.eyJ1IjoiZXZlbnRhc2Vydm8iLCJhIjoiY2s2OGcxaWU5MDRtYzNucWZqdXRicnFpMyJ9.HRdmn4ful40N4svL9ix8vA"
     }).addTo(map)
     L.marker([this.latitude, this.longitude]).addTo(map)
   }
