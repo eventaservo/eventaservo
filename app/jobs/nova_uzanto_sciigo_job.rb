6a60f9ac517b500672bc3d7fba822ead6c9ac475
Fernando Ŝajani <shayani@gmail.com>
2020-06-03 10:42:33 -0300

    Cimriparo pri kreado de nova uzanto

--- app/jobs/nova_uzanto_sciigo_job.rb
+++ app/jobs/nova_uzanto_sciigo_job.rb
@@ -6,7 +6,7 @@ class NovaUzantoSciigoJob < ApplicationJob
 
     mesagho = "Nova uzanto registrita:\n\n"
     mesagho += "#{uzanto.name} el #{uzanto.city} (#{uzanto.country.code.upcase})\n\n"
-    mesagho += events_by_username_url(uzanto.username)
+    mesagho += events_by_username_url(uzanto.username) unless uzanto.username.nil?
     system "telegram-send --config config/es_admin_channel.conf \"#{mesagho}\""
   end
 end
