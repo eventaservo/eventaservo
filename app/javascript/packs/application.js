fee58a09e6595db5dbf625753c0719450943ab8d
Johano Underwood <kircxo@gmail.com>
2020-07-20 17:25:30 +0200

    Tradukis Trix butonojn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,7 +14,6 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-require("trix")
 require("@rails/actiontext")
 
 function requireAll (r) {
