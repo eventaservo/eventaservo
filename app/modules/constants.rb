a70f08ea6d593f963e9a3b776f34b86833a07354
Fernando Shayani <shayani@gmail.com>
2020-11-15 09:53:20 -0300

    Aldonas ligilon por listigi retajn eventojn per Webcal

--- app/modules/constants.rb
+++ app/modules/constants.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Constants
-  VERSIO = '2.18.6 (beta)'
+  VERSIO = '2.18.7 (beta)'
   public_constant :VERSIO
 
   ADMIN_EMAILS = %w[yves.nevelsteen@gmail.com shayani@icloud.com].freeze
