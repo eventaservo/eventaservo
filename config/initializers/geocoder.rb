bbb26f15e3167633210405ab80bd8aabcdfd2675
Fernando Shayani <shayani@gmail.com>
2021-10-16 18:26:51 -0300

    Ebligas ENV por Google maps key kaj IPInfo key

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -3,10 +3,10 @@ Geocoder.configure(
   # timeout: 3,                 # geocoding service timeout (secs)
   # lookup: :nominatim,         # name of geocoding service (symbol)
   google: {
-    api_key: Rails.application.credentials.google_maps_key
+    api_key: Rails.application.credentials.google_maps_key || ENV["GOOGLE_MAPS_KEY"]
   },
   ipinfo_io: {
-    api_key: Rails.application.credentials.ipinfo_key
+    api_key: Rails.application.credentials.ipinfo_key || ENV["IPINFO_KEY"]
   },
   lookup: :google,
   ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
