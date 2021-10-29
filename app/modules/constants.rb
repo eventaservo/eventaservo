48a59dbee80a6ac0d36448a55d76acc434ca79fc
Fernando Shayani <shayani@gmail.com>
2021-10-29 17:10:53 -0300

    Tajperaro

--- app/modules/constants.rb
+++ app/modules/constants.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Constants
-  VERSIO = '2.23.3'
+  VERSIO = '2.23.4'
   public_constant :VERSIO
 
   ADMIN_EMAILS = %w[yves.nevelsteen@gmail.com shayani@gmail.com].freeze
