4d3730ae4bb9d9031c8204fa0345f0fa804b929f
Fernando Shayani <shayani@gmail.com>
2021-11-25 18:23:28 -0300

    Aldonas Hakiri.io kiel partnera kompanio

--- app/modules/constants.rb
+++ app/modules/constants.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Constants
-  VERSIO = '2.24.5'
+  VERSIO = '2.24.6'
   public_constant :VERSIO
 
   ADMIN_EMAILS = %w[yves.nevelsteen@gmail.com shayani@gmail.com].freeze
