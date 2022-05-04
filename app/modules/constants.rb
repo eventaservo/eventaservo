b2711fbf4d09a94fc7771b8ba40950528102883f
Fernando Shayani <shayani@gmail.com>
2022-05-04 07:09:34 -0300

    Removes parallel testing

--- app/modules/constants.rb
+++ app/modules/constants.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Constants
-  VERSIO = '2.26.3t'
+  VERSIO = '2.26.3b'
   public_constant :VERSIO
 
   ADMIN_EMAILS = %w[yves.nevelsteen@gmail.com shayani@gmail.com].freeze
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,7 +12,6 @@ SimpleCov.start
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  parallelize(workers: 3)
   include FactoryBot::Syntax::Methods
   include ActionMailer::TestHelper
 
