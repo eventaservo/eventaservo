b37b708c7ae228bd5be399dec80a1d7d890a12cf
Fernando Shayani <shayani@gmail.com>
2022-06-04 20:49:19 -0300

    Runs Airbrake only on production

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -9,7 +9,8 @@
 #
 # Configuration details:
 # https://github.com/airbrake/airbrake-ruby#configuration
-if (project_id = ENV['AIRBRAKE_PROJECT_ID']) &&
+if Rails.env.production? &&
+   (project_id = ENV['AIRBRAKE_PROJECT_ID']) &&
    project_key = (ENV['AIRBRAKE_PROJECT_KEY'] || ENV['AIRBRAKE_API_KEY'])
   Airbrake.configure do |c|
     # You must set both project_id & project_key. To find your project_id and
