15976989b1df165850420cde13ed78d7da382bfa
Fernando Shayani <shayani@gmail.com>
2022-07-07 09:50:15 -0300

    Aldonas issue numero al versio

--- app/modules/constants.rb
+++ app/modules/constants.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Constants
-  VERSIO = '2.29.0'
+  VERSIO = '2.29.0-402'
   public_constant :VERSIO
 
   ADMIN_EMAILS = %w[yves.nevelsteen@gmail.com shayani@gmail.com].freeze
