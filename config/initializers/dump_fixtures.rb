f0892b923ad79b432d14676daf7e451805b260a6
Fernando Shayani <shayani@gmail.com>
2022-09-17 09:31:48 +0000

    dump_fixtures method

---
+++ config/initializers/dump_fixtures.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module ActiveRecord
+  class Base
+    def dump_fixture
+      fixture_file = "#{Rails.root}/test/fixtures/#{self.class.table_name}.yml"
+      File.open(fixture_file, 'a+') do |f|
+        f.puts(
+          { "#{self.class.table_name.singularize}_#{id}" => attributes.except('created_at', 'updated_at') }
+            .to_yaml
+            .sub!(/---\s?/, "\n")
+        )
+      end
+    end
+  end
+end
