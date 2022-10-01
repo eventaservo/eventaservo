7bc784bc80cff5c6461085f4a1a3ffa9b52645a0
Fernando Shayani <shayani@gmail.com>
2022-10-01 22:56:24 +0000

    Forviŝas not_online kondiĉo de Internacia Kalendaro

--- app/controllers/internacia_controller.rb
+++ app/controllers/internacia_controller.rb
@@ -5,6 +5,6 @@ class InternaciaController < ApplicationController
     @jaro = params[:jaro].to_i
     redirect_to root_url, flash: { error: 'Jaro ne valida' } and return if (@jaro < 1887 || @jaro > 2100)
 
-    @eventoj = Event.lau_jaro(@jaro).not_online.international_calendar.order(:date_start)
+    @eventoj = Event.lau_jaro(@jaro).international_calendar.order(:date_start)
   end
 end
