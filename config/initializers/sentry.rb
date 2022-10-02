b1fdaf6288242f9111b6014aa2684c94fa873b13
Fernando Shayani <shayani@gmail.com>
2022-10-02 20:22:37 +0000

    Sentry: Change traces_sample_rate to 0.5

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -7,11 +7,11 @@ Sentry.init do |config|
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production.
-  config.traces_sample_rate = 1.0
+  config.traces_sample_rate = 0.5
   # or
-  config.traces_sampler = lambda do |_context|
-    true
-  end
+  # config.traces_sampler = lambda do |_context|
+  #   true
+  # end
 
   config.enabled_environments = %w[production staging]
 end
