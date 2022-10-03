a2dfcec125fb385a5ca06095833d23c95bf60f0b
Fernando Shayani <shayani@gmail.com>
2022-10-03 01:29:13 +0000

    Sentry: Add custom error if user feedback form fails

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -32,7 +32,9 @@ class ErrorsController < ApplicationController
       "Authorization" => "Bearer #{Rails.application.credentials.dig(:sentry, :auth_token)}"
     }
 
-    HTTParty.post(sentry_url, body: body, headers: headers)
+    response = HTTParty.post(sentry_url, body: body, headers: headers)
+
+    Sentry.capture_message "Sentry user feedback form response error" unless response.success?
 
     redirect_to root_path, notice: "Dankon!"
   end
