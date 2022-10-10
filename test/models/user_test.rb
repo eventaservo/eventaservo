aa65e5df6a29a259d785ccf839c89a247f6d4c27
Fernando Shayani <shayani@gmail.com>
2022-10-10 12:00:29 +0000

    Organize User#merge_tests in specific folder

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -41,36 +41,6 @@ class UserTest < ActiveSupport::TestCase
     assert_not user.destroy
   end
 
-  test "kunigas uzant-kontojn" do
-    user1 = create(:uzanto)
-    user2 = create(:uzanto)
-
-    assert user1.events.count, 0
-    create(:evento, user_id: user1.id)
-    assert user1.events.count, 1
-
-    assert user1.organizoj.count, 0
-    organization = create(:organizo)
-    OrganizationUser.create(organization_id: organization.id, user_id: user1.id)
-    assert user1.organizoj.count, 1
-
-    assert_not user1.destroy
-
-    assert user2.events.count, 0
-    assert user2.organizoj.count, 0
-    user1.merge_to(user2.id)
-    assert user2.events.count, 1
-    assert user2.organizoj.count, 1
-
-    assert user1.destroy
-    assert_not user2.destroy
-  end
-
-  test "ne eblas kunigi la saman konton" do
-    user = create(:uzanto)
-    assert_not user.merge_to(user.id)
-  end
-
   test "JWT Token must be created automatically for new users" do
     user = FactoryBot.build(:user)
 
