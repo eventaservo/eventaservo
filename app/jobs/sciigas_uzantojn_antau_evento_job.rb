4ce02ff59f7bac613541b65535028158a8b86665
Fernando Shayani <shayani@gmail.com>
2022-10-18 17:01:56 -0300

    Rescue exception for NoMethodError: undefined method `participants' for nil:NilClass #441
    Fixes EVENTA-SERVO-AA
    Closes #411

--- app/jobs/sciigas_uzantojn_antau_evento_job.rb
+++ app/jobs/sciigas_uzantojn_antau_evento_job.rb
@@ -4,12 +4,12 @@ class SciigasUzantojnAntauEventoJob < ApplicationJob
   def perform(code, reminder_date_string = "2.hours")
     e = Event.by_code(code)
 
-    unless e.participants.any?
-      logger.info "[INFO] Evento #{code} ne havas partoprenontoj. Rememoriga mesaĝo ne sendita."
-      return
+    begin
+      return unless e.participants.any?
+    rescue => e
+      Sentry.capture_exception(e)
     end
 
-    logger.info "[INFO] Partoprenontoj de evento #{code} trovita. Sendante rememorigan mesaĝon."
     EventMailer.rememorigas_uzantojn_pri_evento(e.id, reminder_date_string).deliver_later
   end
 end
