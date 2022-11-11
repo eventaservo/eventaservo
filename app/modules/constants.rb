66262a3d578797cbea717e55f1fca03afd3b59f6
Fernando Shayani <shayani@gmail.com>
2022-11-10 21:35:48 -0300

    Github Actions update

--- app/modules/constants.rb
+++ app/modules/constants.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Constants
-  VERSIO = "2.38.1"
+  VERSIO = "2.39.0d"
   public_constant :VERSIO
 
   ADMIN_EMAILS = %w[yves.nevelsteen@gmail.com shayani@gmail.com].freeze
