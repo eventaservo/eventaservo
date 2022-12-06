b548f41b184d762f0e1168b344e9ebf1c0f93b6f
Fernando Shayani <shayani@gmail.com>
2022-12-06 06:35:40 -0300

    Changes delayed_job max_attemps to 8 (~1 hour)

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,6 +1,6 @@
 Delayed::Worker.destroy_failed_jobs     = false
 Delayed::Worker.sleep_delay             = 60
-Delayed::Worker.max_attempts            = 3
+Delayed::Worker.max_attempts            = 8
 Delayed::Worker.max_run_time            = 5.minutes
 Delayed::Worker.delay_jobs              = !Rails.env.test?
 Delayed::Worker.raise_signal_exceptions = :term
