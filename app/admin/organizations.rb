ea50034507d4486826405e13f768db99e9a4d37d
Fernando Shayani <shayani@gmail.com>
2022-12-29 19:08:40 -0300

    AA: Customize Organizations page

--- app/admin/organizations.rb
+++ app/admin/organizations.rb
@@ -17,7 +17,7 @@ ActiveAdmin.register Organization do
 
   actions :all, except: :destroy
 
-  filter :name_cont
+  filter :name_cont, label: "Name"
   filter :country
 
   includes :country
@@ -26,11 +26,12 @@ ActiveAdmin.register Organization do
 
   index do
     id_column
-    column :name
+    column("Name") { |o| link_to(o.name, "/o/#{o.short_name}", target: :_blank, rel: :noopener)}
     column :short_name
     column :country
     column :city
     column("Users") { |o| o.users.count }
+    column("Events") { |o| o.events.count }
   end
 
   show do
@@ -45,6 +46,13 @@ ActiveAdmin.register Organization do
             column("User") do |user|
               link_to user.name, active_admin_user_path(user)
             end
+          end
+        end
+
+        panel "Events" do
+          table_for resource.events.order(:date_start) do
+            column("Event") { |event| link_to event.title, active_admin_event_path(event) }
+            column :date_start
           end
         end
       end
