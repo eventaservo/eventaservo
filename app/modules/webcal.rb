0c20b6e5a51dd53063a666f5d4ec571cbdcee00b
Fernando Shayani <shayani@gmail.com>
2023-01-17 06:53:53 -0300

    Create whole-day calendar event

--- app/modules/webcal.rb
+++ app/modules/webcal.rb
@@ -17,6 +17,8 @@ module Webcal
     def kreas_eventon(icalendar, evento)
       if evento.multtaga?
         kreas_multtagan_eventon(icalendar, evento)
+      elsif evento.tuttaga?
+        create_whole_day_event(icalendar, evento)
       else
         icalendar.event do |e|
           e.dtstart = Icalendar::Values::DateOrDateTime.new(
@@ -75,6 +77,26 @@ module Webcal
         e.summary     = "Fino: #{evento.title}"
         e.description = evento.description + '\n\n' + event_url(evento.ligilo)
         e.location    = evento.full_address
+      end
+    end
+
+    # Creates a whole-day event
+    #
+    # @params [Icalendar::Calendar] icalendar
+    # @params [Event] event
+    #
+    # @return [Icalendar::Event]
+    def create_whole_day_event(icalendar, event)
+      icalendar.event do |e|
+        e.dtstart = Icalendar::Values::DateOrDateTime.new(
+          event.date_start.in_time_zone(event.time_zone).strftime("%Y%m%d"), tzid: event.time_zone
+        ).call
+        e.dtend = Icalendar::Values::DateOrDateTime.new(
+          (event.date_end.in_time_zone(event.time_zone) + 1.day).strftime("%Y%m%d"), tzid: event.time_zone
+        ).call
+        e.summary = event.title
+        e.description = "#{event.description}\n\n#{event_url(event.ligilo)}"
+        e.location = event.full_address
       end
     end
 end
