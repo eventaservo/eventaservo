76f735d1e48aec59fda9550115260b04c3a07854
Fernando Shayani <shayani@gmail.com>
2023-01-30 05:57:13 -0300

    Removes unused gem Minitest Reporters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,9 +4,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 require 'minitest/autorun'
-require "minitest/reporters"
-
-Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # SimpleCov
 require 'simplecov'
