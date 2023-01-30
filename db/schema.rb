0d31ffa9460d65fc0e9343d17439a2bdf49394f4
Fernando Shayani <shayani@gmail.com>
2023-01-30 05:15:00 -0300

    Upgrade to Rails 7.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_15_093811) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_01_14_210855) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "unaccent"
@@ -22,8 +21,8 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
     t.text "body"
     t.string "record_type", null: false
     t.bigint "record_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
@@ -34,8 +33,8 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
     t.bigint "resource_id"
     t.string "author_type"
     t.bigint "author_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
@@ -46,7 +45,7 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -58,7 +57,7 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
     t.text "metadata"
     t.bigint "byte_size", null: false
     t.string "checksum"
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
@@ -72,8 +71,8 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
   create_table "ads", force: :cascade do |t|
     t.bigint "event_id"
     t.boolean "active", default: true
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "url"
     t.index ["event_id"], name: "index_ads_on_event_id"
   end
@@ -83,7 +82,7 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
     t.bigint "user_id"
     t.string "name"
     t.jsonb "properties"
-    t.datetime "time"
+    t.datetime "time", precision: nil
     t.index ["name", "time"], name: "index_ahoy_events_on_name_and_time"
     t.index ["properties"], name: "index_ahoy_events_on_properties", opclass: :jsonb_path_ops, using: :gin
     t.index ["user_id"], name: "index_ahoy_events_on_user_id"
@@ -115,7 +114,7 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
     t.string "app_version"
     t.string "os_version"
     t.string "platform"
-    t.datetime "started_at"
+    t.datetime "started_at", precision: nil
     t.index ["user_id"], name: "index_ahoy_visits_on_user_id"
     t.index ["visit_token"], name: "index_ahoy_visits_on_visit_token", unique: true
   end
@@ -128,16 +127,16 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
     t.string "country"
     t.string "path"
     t.string "vidmaniero"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "city"
     t.string "region"
   end
 
   create_table "countries", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "code"
     t.string "continent"
     t.index ["continent"], name: "index_countries_on_continent"
@@ -150,13 +149,13 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
     t.integer "attempts", default: 0, null: false
     t.text "handler", null: false
     t.text "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
+    t.datetime "run_at", precision: nil
+    t.datetime "locked_at", precision: nil
+    t.datetime "failed_at", precision: nil
     t.string "locked_by"
     t.string "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
@@ -168,11 +167,11 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
     t.text "city"
     t.integer "country_id", null: false
     t.integer "user_id", null: false
-    t.datetime "date_start", null: false
-    t.datetime "date_end"
+    t.datetime "date_start", precision: nil, null: false
+    t.datetime "date_end", precision: nil
     t.string "code", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "site"
     t.string "email"
     t.boolean "deleted", default: false, null: false
@@ -208,8 +207,8 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
     t.string "followable_type", null: false
     t.bigint "followable_id", null: false
     t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["followable_type", "followable_id"], name: "index_followers_on_followable_type_and_followable_id"
     t.index ["user_id"], name: "index_followers_on_user_id"
   end
@@ -218,8 +217,8 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
     t.string "likeable_type", null: false
     t.bigint "likeable_id", null: false
     t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["likeable_type", "likeable_id"], name: "index_likes_on_likeable_type_and_likeable_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
@@ -228,8 +227,8 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
     t.integer "country_id", null: false
     t.string "email", null: false
     t.string "code", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["code"], name: "index_notification_lists_on_code"
     t.index ["country_id", "email"], name: "index_notification_lists_on_country_id_and_email"
   end
@@ -237,8 +236,8 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
   create_table "organization_events", force: :cascade do |t|
     t.bigint "organization_id"
     t.bigint "event_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["event_id"], name: "index_organization_events_on_event_id"
     t.index ["organization_id"], name: "index_organization_events_on_organization_id"
   end
@@ -247,8 +246,8 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
     t.bigint "organization_id"
     t.bigint "user_id"
     t.boolean "admin", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["admin"], name: "index_organization_users_on_admin"
     t.index ["organization_id"], name: "index_organization_users_on_organization_id"
     t.index ["user_id"], name: "index_organization_users_on_user_id"
@@ -259,8 +258,8 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
     t.string "short_name", null: false
     t.boolean "official", default: false
     t.string "logo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "email"
     t.string "url"
     t.integer "country_id", default: 99999
@@ -277,8 +276,8 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
   create_table "participants", force: :cascade do |t|
     t.bigint "event_id"
     t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.boolean "public", default: false
     t.index ["event_id"], name: "index_participants_on_event_id"
     t.index ["public"], name: "index_participants_on_public"
@@ -292,7 +291,7 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
     t.string "tagger_type"
     t.integer "tagger_id"
     t.string "context", limit: 128
-    t.datetime "created_at"
+    t.datetime "created_at", precision: nil
     t.index ["context"], name: "index_taggings_on_context"
     t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
     t.index ["tag_id"], name: "index_taggings_on_tag_id"
@@ -313,30 +312,30 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
   create_table "timezones", force: :cascade do |t|
     t.string "en", null: false
     t.string "eo", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
     t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
+    t.datetime "confirmed_at", precision: nil
+    t.datetime "confirmation_sent_at", precision: nil
     t.string "unconfirmed_email"
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "locked_at", precision: nil
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.boolean "admin", default: false
     t.string "name"
     t.string "image"
@@ -383,7 +382,7 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
     t.string "event", null: false
     t.string "whodunnit"
     t.text "object"
-    t.datetime "created_at"
+    t.datetime "created_at", precision: nil
     t.text "object_changes"
     t.integer "transaction_id"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
@@ -395,8 +394,8 @@ ActiveRecord::Schema.define(version: 2023_01_15_093811) do
     t.string "url", null: false
     t.string "title"
     t.string "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
