d4b67e91e49eb86b08bcb027422c0a616378f48d
Fernando Shayani <shayani@gmail.com>
2023-02-19 16:25:24 -0300

    Replaces check for short_url availability from Vue to Turbo

--- app/javascript/src/main.js
+++ app/javascript/src/main.js
@@ -1,26 +1,13 @@
-import Vue from "vue/dist/vue.esm"
-import MapoVidmaniero from "../components/MapoVidmaniero"
-import Mallongilo from "../components/eventoj/Mallongilo"
+import Vue from 'vue/dist/vue.esm'
+import MapoVidmaniero from '../components/MapoVidmaniero'
 
-
-document.addEventListener("DOMContentLoaded", () => {
-
-  if (document.getElementById("vue-mapo-vidmaniero")) {
+document.addEventListener('DOMContentLoaded', () => {
+  if (document.getElementById('vue-mapo-vidmaniero')) {
     const mapoVidManiero = new Vue({
-      el: "#vue-mapo-vidmaniero",
-      name: "Mapo vidmaniero",
+      el: '#vue-mapo-vidmaniero',
+      name: 'Mapo vidmaniero',
       components: {
         MapoVidmaniero
-      }
-    })
-  }
-
-  if (document.getElementById("vue-mallongilo")) {
-    const mallongilo = new Vue({
-      el: "#vue-mallongilo",
-      name: "Mallongilo",
-      components: {
-        Mallongilo
       }
     })
   }
