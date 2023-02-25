a4c2552c7f6c72ab256b099623b3da4d8118e80f
Fernando Shayani <shayani@gmail.com>
2023-02-25 05:43:22 -0300

    fix: run search after 2.5 seconds

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,17 +1,23 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
 
 // Connects to data-controller="search"
 export default class extends Controller {
-  static targets = [ "searchTerm" ]
+  static targets = ['searchTerm']
 
-  connect() {
-  }
+  connect() {}
 
   goToSearchPage(event) {
+    const searchTerm = this.searchTermTarget.value
+    const url = `/serchilo?query=${searchTerm}`
+
+    clearTimeout(this._timer)
+
     if (event.keyCode === 13) {
-      const searchTerm = this.searchTermTarget.value
-      const url = `/serchilo?query=${searchTerm}`
       window.location.href = url
     }
+
+    this._timer = setTimeout(() => {
+      window.location.href = url
+    }, 2500)
   }
 }
