75b1921967d53a54327a19dcfe3ea6c14bdbac19
Fernando Shayani <shayani@gmail.com>
2023-03-05 05:33:05 -0300

    refactor(Stimulus): :sparkles: Map view style

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,16 +2,16 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from './application'
+import { application } from "./application"
 
-import EventMapController from './event_map_controller'
-application.register('event-map', EventMapController)
+import EventMapController from "./event_map_controller"
+application.register("event-map", EventMapController)
 
-import EventUserInterestedButtonController from './event_user_interested_button_controller'
-application.register(
-  'event-user-interested-button',
-  EventUserInterestedButtonController
-)
+import EventUserInterestedButtonController from "./event_user_interested_button_controller"
+application.register("event-user-interested-button", EventUserInterestedButtonController)
 
-import UserController from './user_controller'
-application.register('user', UserController)
+import MapViewController from "./map_view_controller"
+application.register("map-view", MapViewController)
+
+import UserController from "./user_controller"
+application.register("user", UserController)
