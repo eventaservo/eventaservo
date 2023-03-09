88a3808f61355fa94d0525b0a403e6ab7cdf7f79
Fernando Shayani <shayani@gmail.com>
2023-03-09 05:46:03 -0300

    test(Cypress): add Faker package

--- cypress/e2e/events/create_new_events.cy.js
+++ cypress/e2e/events/create_new_events.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+import { faker } from '@faker-js/faker'
+
 describe('Create events', () => {
   // beforeEach(() => {
   // Cypress starts out with a blank slate for each test
@@ -11,8 +13,9 @@ describe('Create events', () => {
   it('with basic information', () => {
     cy.login()
     cy.visit('https://localhost:3000/e/new')
-    cy.get('#event_title').type('Testa evento')
-    cy.get('#event_description').type('Testa evento priskribo')
+    cy.get('#event_title').type(faker.commerce.product())
+    cy.get('#event_description').type(faker.commerce.productName())
+    cy.get('#event_enhavo').type(faker.commerce.productDescription())
     cy.get(':nth-child(2) > .btn').click()
     cy.get('#event_site').type('uea.org')
     cy.get('.button-submit').click()
