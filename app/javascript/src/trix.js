130eea07dd3dfc0b0defd05254489f9c6bb6f833
Fernando Shayani <shayani@gmail.com>
2023-03-09 05:34:39 -0300

    fix(Trix): Javascript console error

--- app/javascript/src/trix.js
+++ app/javascript/src/trix.js
@@ -1,62 +1,76 @@
-import Trix from "trix"
+import Trix from 'trix'
 
 Trix.config.lang = {
-  attachFiles: "Alkroĉi dosierojn",
-  bold: "Grasigi",
-  bullets: "Bula listo",
-  byte: "bajto",
-  bytes: "bajtoj",
-  captionPlaceholder: "Aldoni apudskribon…",
-  code: "Kodo",
-  heading1: "Titolo",
-  indent: "Altigi nivelon",
-  italic: "Kursivigi",
-  link: "Ligi",
-  numbers: "Numera listo",
-  outdent: "Malaltigi nivelon",
-  quote: "Citaĵo",
-  redo: "Refari",
-  remove: "Forigi",
-  strike: "Trastreki",
-  undo: "Malfari",
-  unlink: "Malligi",
-  url: "Ligilo",
-  urlPlaceholder: "Entajpu ligilon…",
-  GB: "GB",
-  KB: "kB",
-  MB: "MB",
-  PB: "PB",
-  TB: "TB"
+  attachFiles: 'Alkroĉi dosierojn',
+  bold: 'Grasigi',
+  bullets: 'Bula listo',
+  byte: 'bajto',
+  bytes: 'bajtoj',
+  captionPlaceholder: 'Aldoni apudskribon…',
+  code: 'Kodo',
+  heading1: 'Titolo',
+  indent: 'Altigi nivelon',
+  italic: 'Kursivigi',
+  link: 'Ligi',
+  numbers: 'Numera listo',
+  outdent: 'Malaltigi nivelon',
+  quote: 'Citaĵo',
+  redo: 'Refari',
+  remove: 'Forigi',
+  strike: 'Trastreki',
+  undo: 'Malfari',
+  unlink: 'Malligi',
+  url: 'Ligilo',
+  urlPlaceholder: 'Entajpu ligilon…',
+  GB: 'GB',
+  KB: 'kB',
+  MB: 'MB',
+  PB: 'PB',
+  TB: 'TB',
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  const atribuoj = {
-    bold: "Grasigi",
-    italic: "Kursivigi",
-    strike: "Trastreki",
-    link: "Ligi",
-    "heading-1": "Titolo",
-    quote: "Citaĵo",
-    code: "Kodo",
-    "bullet-list": "Bula listo",
-    "number-list": "Numera listo",
-    "decrease-nesting-level": "Malaltigi nivelon",
-    "increase-nesting-level": "Altigi nivelon",
-    attach: "Alkroĉi dosierojn",
-    undo: "Malfari",
-    redo: "Refari"
-  }
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    if (document.querySelector('.trix-button-row')) {
+      const atribuoj = {
+        bold: 'Grasigi',
+        italic: 'Kursivigi',
+        strike: 'Trastreki',
+        link: 'Ligi',
+        'heading-1': 'Titolo',
+        quote: 'Citaĵo',
+        code: 'Kodo',
+        'bullet-list': 'Bula listo',
+        'number-list': 'Numera listo',
+        'decrease-nesting-level': 'Malaltigi nivelon',
+        'increase-nesting-level': 'Altigi nivelon',
+        attach: 'Alkroĉi dosierojn',
+        undo: 'Malfari',
+        redo: 'Refari',
+      }
 
-  for (const [key, value] of Object.entries(atribuoj)) {
-    document.querySelector(`.trix-button--icon-${key}`).setAttribute("title", value)
-  }
+      for (const [key, value] of Object.entries(atribuoj)) {
+        document
+          .querySelector(`.trix-button--icon-${key}`)
+          .setAttribute('title', value)
+      }
 
-  const ligilajKampoj = document.querySelector(".trix-dialog__link-fields")
+      const ligilajKampoj = document.querySelector('.trix-dialog__link-fields')
 
-  // Por entajpi ligilojn
-  ligilajKampoj.querySelector(".trix-input--dialog").setAttribute("placeholder", "Entajpu ligilon…")
-  ligilajKampoj.querySelector("[data-trix-method='setAttribute']").setAttribute("value", "Ligi")
-  ligilajKampoj.querySelector("[data-trix-method='removeAttribute']").setAttribute("value", "Malligi")
-}, false)
+      // Por entajpi ligilojn
+      ligilajKampoj
+        .querySelector('.trix-input--dialog')
+        .setAttribute('placeholder', 'Entajpu ligilon…')
+      ligilajKampoj
+        .querySelector("[data-trix-method='setAttribute']")
+        .setAttribute('value', 'Ligi')
+      ligilajKampoj
+        .querySelector("[data-trix-method='removeAttribute']")
+        .setAttribute('value', 'Malligi')
+    }
+  },
+  false
+)
 
 export default Trix
