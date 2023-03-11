31e8287bafeb69e55b6c040bc1425274db314004
Fernando Shayani <shayani@gmail.com>
2023-03-11 05:37:38 -0300

    feat(Ahoy): track video events

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -2,6 +2,7 @@
 
 class VideoController < ApplicationController
   def index
+    ahoy.track "Visit Registritaj prezentoj page"
     @videoj = ::Video.joins(:evento).order("events.date_start DESC")
     @pagy, @videoj = pagy(@videoj, items: 6)
   end
@@ -11,8 +12,9 @@ class VideoController < ApplicationController
   end
 
   def create
+    event = Event.lau_ligilo(params[:event_code])
     params.permit(:event_code, :video_link, :title, :description, :image)
-    new_video = Video.create(evento: Event.lau_ligilo(params[:event_code]),
+    new_video = Video.create(evento: event,
       url: params[:video_link],
       title: params[:title].html_safe,
       description: params[:description].html_safe)
@@ -20,6 +22,7 @@ class VideoController < ApplicationController
       new_video.bildo.attach(params[:image])
     end
 
+    ahoy.track "Add video to event", event_url: event.short_url, video_url: new_video.url
     redirect_to event_url(params[:event_code])
   end
 
@@ -27,6 +30,7 @@ class VideoController < ApplicationController
     video = Video.find(params[:id])
     if user_can_edit_event?(user: @current_user, event: video.evento)
       video.destroy
+      ahoy.track "Delete video from event", event_url: video.evento.short_url, video_url: video.url
       flash[:success] = "Video forigita"
     else
       flash[:error] = "Vi ne rajtas forigi tiun videon"
