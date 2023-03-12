6bff7df8272ed43989c409a80e390c6d0be629ab
Fernando Shayani <shayani@gmail.com>
2023-03-11 21:26:38 -0300

    roda o cypress

--- ci.sh
+++ ci.sh
@@ -1,4 +1,5 @@
 bundle exec rails db:migrate
 bundle exec rails db:seed:replant
 bundle exec rails server -b "ssl://0.0.0.0:3000?key=certs/localhost.key&cert=certs/localhost.crt" &
+sleep 5
 npx cypress run
