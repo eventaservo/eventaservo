b111da5f3f1391353160eefd369cf7d170fcb4f8
Fernando Shayani <shayani@gmail.com>
2023-03-13 07:09:42 -0300

    fix: Validi mallongan ligilon #429

--- app/modules/constants.rb
+++ app/modules/constants.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Constants
-  VERSIO = "2.61.0"
+  VERSIO = "2.61.1"
   public_constant :VERSIO
 
   ADMIN_EMAILS = %w[yves.nevelsteen@gmail.com shayani@gmail.com].freeze
