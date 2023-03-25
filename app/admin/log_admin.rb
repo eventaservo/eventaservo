5708f7e6bb3021257ecc325a93ff3553ae0eb34f
Fernando Shayani <shayani@gmail.com>
2023-03-25 06:47:58 -0300

    Improves Log admin page

--- app/admin/log_admin.rb
+++ app/admin/log_admin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Log do
-  actions :all, except: %i[new edit]
+  actions :all, except: %i[new edit destroy]
 
   includes :user
 
@@ -13,6 +13,12 @@ ActiveAdmin.register Log do
     column :user
     column :text
 
-    actions
+    actions defaults: true do |log|
+      event = Event.find_by(id: log.event_id)
+      organization = Organization.find_by(id: log.organization_id)
+
+      span link_to "Event", event_url(event.ligilo), target: "_blank" if event
+      span link_to "Organization", organization_url(organization.short_name), target: "_blank" if organization
+    end
   end
 end
