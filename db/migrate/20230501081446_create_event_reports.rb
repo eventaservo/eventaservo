533d9c851e11ad69e861d13fbc341926f9b4b627
Fernando Shayani <shayani@gmail.com>
2023-05-03 21:16:41 -0300

    Create `event_reports` table #526

---
+++ db/migrate/20230501081446_create_event_reports.rb
@@ -0,0 +1,11 @@
+class CreateEventReports < ActiveRecord::Migration[7.0]
+  def change
+    create_table :event_reports do |t|
+      t.references :event, null: false
+      t.string :title, null: false
+      t.references :user, null: false
+
+      t.timestamps
+    end
+  end
+end
