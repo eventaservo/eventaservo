70b0c98810d721fc72a0f57a2edfeecf334057f1
Fernando Shayani <shayani@gmail.com>
2023-05-29 06:42:46 -0300

    Update gems

--- app/modules/constants.rb
+++ app/modules/constants.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Constants
-  VERSIO = "2.72.2"
+  VERSIO = "2.73.0"
   public_constant :VERSIO
 
   ADMIN_EMAILS = %w[yves.nevelsteen@gmail.com shayani@gmail.com].freeze
