8e261fec04e9360c242ceda09bc24bdba65bbffe
Fernando Shayani <shayani@gmail.com>
2023-07-31 21:20:10 -0300

    doc(API): events by category #539

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxMjU4MA/spectral.js?branch_slug=main&token=1922566a-9bbd-49cd-9661-49a533ba659a";
+export default { extends: ruleset };
