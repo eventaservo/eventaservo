3156d86ced3485361e1e4eed1b6775c1bebcd61e
Fernando Shayani <shayani@gmail.com>
2023-10-19 06:24:03 -0300

    refactor: Removes annotation from test files

--- test/models/event/report_test.rb
+++ test/models/event/report_test.rb
@@ -1,20 +1,3 @@
-# == Schema Information
-#
-# Table name: event_reports
-#
-#  id         :bigint           not null, primary key
-#  title      :string
-#  url        :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  event_id   :bigint           not null
-#  user_id    :bigint           not null
-#
-# Indexes
-#
-#  index_event_reports_on_event_id  (event_id)
-#  index_event_reports_on_user_id   (user_id)
-#
 require "test_helper"
 
 class Event::ReportTest < ActiveSupport::TestCase
