de45e2b2a94f21ab848b05a62a716ebfe422722b
Fernando Shayani <shayani@gmail.com>
2023-10-27 15:21:14 -0300

    Update Redis envs

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -14,7 +14,7 @@ Geocoder.configure(
   use_https: true,              # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  cache: Redis.new(host: ENV["REDIS_SERVER"]),             # cache object (must respond to #[], #[]=, and #del)
+  cache: Redis.new(url: ENV["REDIS_URL"]),             # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
   # Exceptions that should not be rescued by default
