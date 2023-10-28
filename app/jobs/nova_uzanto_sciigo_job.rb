815b846342c0825c52873bdbc8acae89d5d659ba
Fernando Shayani <shayani@gmail.com>
2023-10-28 05:23:53 -0300

    Removes Telegram queue

--- app/jobs/nova_uzanto_sciigo_job.rb
+++ app/jobs/nova_uzanto_sciigo_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class NovaUzantoSciigoJob < ApplicationJob
-  queue_as :telegram
+  queue_as :low
 
   def perform(uzanto)
     return true if Rails.env.test? || Rails.env.development?
