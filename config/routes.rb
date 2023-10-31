08a2f73ff7c85d31fb5288ebce0294378868d4af
Fernando Shayani <shayani@gmail.com>
2023-10-31 06:04:51 -0300

    Basic statistics page

--- config/routes.rb
+++ config/routes.rb
@@ -122,6 +122,7 @@ Rails.application.routes.draw do
     get "eventoj", controller: "events", action: :index
     get "forigitaj_eventoj", controller: "events", action: :deleted
     get "senlokaj_eventoj", to: "events#senlokaj_eventoj"
+    get "statistics", controller: "statistics", action: :index
     patch "forigitaj_eventoj/restauri/:event_code", controller: "events", action: :recover, as: "recover_event"
     resources :reklamoj do
       get "toggle_active"
