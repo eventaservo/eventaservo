2ef962a4b1bca5a9900c09c05ab6d228bfc91d49
Fernando Shayani <shayani@gmail.com>
2023-12-02 20:07:22 -0300

    fix: Send user feedback when error occurs

--- test/mailers/previews/admin_mailer_preview.rb
+++ test/mailers/previews/admin_mailer_preview.rb
@@ -2,7 +2,7 @@
 
 # Preview all emails at http://localhost:3000/rails/mailers/event_mailer
 class AdminMailerPreview < ActionMailer::Preview
-  def informas_admin_pri_teksto
-    AdminMailer.informas("PROV-TEKSTO al Administratoj")
+  def notify
+    AdminMailer.notify(subject: "[ES] User reported error", body: "PROV-TEKSTO al Administratoj")
   end
 end
