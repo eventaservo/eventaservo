8bda744070584e4bebb06cfaa2b3f2bceb3a33f9
Fernando Shayani <shayani@gmail.com>
2023-12-03 07:17:22 -0300

    refactor: Move API integration tests to RSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require_relative "../config/environment"
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
+require "debug"
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
