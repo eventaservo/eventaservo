32fbf0d9dff2c5c9120b1069757fa40269257ef9
Fernando Shayani <shayani@gmail.com>
2024-01-11 08:03:02 -0300

    fix: ActiveAdmin Ransack methods

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -39,4 +39,8 @@ class Ahoy::Visit < ApplicationRecord
 
   has_many :events, class_name: "Ahoy::Event", dependent: :destroy
   belongs_to :user, optional: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["app_version", "browser", "city", "country", "device_type", "id", "id_value", "ip", "landing_page", "latitude", "longitude", "os", "os_version", "platform", "referrer", "referring_domain", "region", "started_at", "user_agent", "user_id", "utm_campaign", "utm_content", "utm_medium", "utm_source", "utm_term", "visit_token", "visitor_token"]
+  end
 end
