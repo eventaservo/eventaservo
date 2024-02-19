7a54f73c3de13633cf0d400ade5335f4cc72ba37
Fernando Shayani <shayani@gmail.com>
2024-02-19 19:54:48 -0300

    Add model comments for factory :event_redirections

--- spec/factories/event_redirections.rb
+++ spec/factories/event_redirections.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: event_redirections
+#
+#  id            :bigint           not null, primary key
+#  hits          :integer          default(0), not null
+#  new_short_url :string           not null
+#  old_short_url :string           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+# Indexes
+#
+#  index_event_redirections_on_old_short_url  (old_short_url) UNIQUE
+#
 FactoryBot.define do
   factory :event_redirection do
     old_short_url { "old_short_url" }
