76ec36676a121214cea742848f62e1efbd34d739
Fernando Shayani <shayani@gmail.com>
2024-04-26 08:19:41 -0300

    Copy translations to `en` and `ptbr` locales

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Eventaservo
     config.time_zone = "UTC"
 
     config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.{rb,yml}")]
-    config.i18n.available_locales = [:eo, :en, :es, :fr, :nl, :ptbr]
+    config.i18n.available_locales = [:eo, :en, :ptbr]
     config.i18n.default_locale = :eo
 
     config.exceptions_app = routes
