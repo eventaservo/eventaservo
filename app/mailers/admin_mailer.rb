3c71c5e8ed685f57acd9d646bda5263a1fa11164
Fernando Shayani <shayani@gmail.com>
2024-05-13 07:32:57 -0300

    chore(DevOps): Send email to admin on server startup

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,4 +4,15 @@ class AdminMailer < ApplicationMailer
   def notify(subject:, body:)
     mail(to: Constants::ADMIN_EMAILS, subject:, body:)
   end
+
+  # Notifies the DevOps team that the application has started
+  def startup_notifier
+    to = "fernando@eventaservo.org"
+    date = Time.zone.now.strftime("%d/%m/%Y %H:%M")
+    host = Socket.gethostname
+    subject = "[EventaServo] #{Rails.env.titleize} #{Eventaservo::Application::VERSION} started"
+    body = "#{host} | #{date}"
+
+    mail(to:, subject:, body:)
+  end
 end
