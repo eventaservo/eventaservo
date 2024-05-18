a3d779d3d14ac7d5801e17095b651520b9bb8522
Fernando Shayani <shayani@gmail.com>
2024-05-18 20:29:32 -0300

    Installs Telegram Simple Messenger (#703)
    and use it to notify when the server starts

--- config/initializers/startup_notifier.rb
+++ config/initializers/startup_notifier.rb
@@ -1,5 +1,8 @@
 Rails.application.config.after_initialize do
-  if Rails.env.production?
-    AdminMailer.startup_notifier.deliver_now
+  if Rails.env.in? %w[production staging]
+    message = "#{Rails.env.titleize} #{Eventaservo::Application::VERSION} started"
+    TelegramSimpleMessenger.send_message(message)
   end
+rescue => e
+  Sentry.capture_exception(e)
 end
---
+++ config/initializers/telegram_simple_messenger.rb
@@ -0,0 +1,2 @@
+TelegramSimpleMessenger.default_api_key = ENV["TELEGRAM_API_KEY"]
+TelegramSimpleMessenger.default_chat_id = ENV["TELEGRAM_CHAT_ID"]
