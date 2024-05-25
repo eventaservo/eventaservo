b325c09cc46e215f7482f5d933a56bd8d1ba969f
Fernando Shayani <shayani@gmail.com>
2024-05-25 20:58:31 -0300

    Gem: update `invisible_captcha`

--- config/initializers/invisible_captcha.rb
+++ config/initializers/invisible_captcha.rb
@@ -1,4 +1,6 @@
 InvisibleCaptcha.setup do |config|
-  config.timestamp_enabled = false if Rails.env == "test"
+  config.timestamp_enabled = !Rails.env.test?
+  config.spinner_enabled = !Rails.env.test?
+
   config.injectable_styles = true
 end
