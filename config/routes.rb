37913d6acc8f40c4de676f5fb9e0c5edf8870810
Fernando Shayani <shayani@gmail.com>
2024-06-24 16:22:36 +0000

    Adds locale path to continent/country/city

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,13 @@ Rails.application.routes.draw do
 
   ActiveAdmin.routes(self)
 
+  devise_for :users, controllers: {sessions: "users/sessions",
+                                   registrations: "users/registrations",
+                                   omniauth_callbacks: "users/omniauth_callbacks",
+                                   passwords: "users/passwords"}
+
+  draw(:api)
+
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     root to: "home#index"
 
@@ -40,15 +47,14 @@ Rails.application.routes.draw do
     get "/eventoj-hu", to: redirect("/ik/jaroj#eventoj-hu")
 
     draw(:videos)
+
+    # Countries and cities
+    get "/:continent", to: "events#by_continent", as: "events_by_continent"
+    get "/:continent/:country_name", controller: "events", action: "by_country", as: "events_by_country"
+    get "/:continent/:country_name/:city_name", controller: "events", action: "by_city", as: "events_by_city"
   end
 
-  devise_for :users, controllers: {sessions: "users/sessions",
-                                   registrations: "users/registrations",
-                                   omniauth_callbacks: "users/omniauth_callbacks",
-                                   passwords: "users/passwords"}
-
   draw(:shortcuts)
-  draw(:api)
 
   # iloj
   namespace :iloj do
@@ -85,8 +91,28 @@ Rails.application.routes.draw do
   get "/sciigo/:recipient_code/forigu", controller: "notification_list", action: :delete, as: "delete_recipient"
   post "/sciigo", controller: "notification_list", action: :create, as: "new_recipient"
 
-  # Countries and cities
-  get "/:continent", to: "events#by_continent", as: "events_by_continent"
-  get "/:continent/:country_name", controller: "events", action: "by_country", as: "events_by_country"
-  get "/:continent/:country_name/:city_name", controller: "events", action: "by_city", as: "events_by_city"
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    root to: "home#index"
+
+    # Anoncoj kaj Konkursoj
+    get "/anoncoj", to: "home#anoncoj", as: "anoncoj"
+
+    # International Calendar (Internacia kalendaro)
+    get "ik", to: "international_calendar#index", as: "international_calendar"
+    get "ik/jaroj", to: "international_calendar#year_list", as: "international_calendar_year_list"
+    get "ik/:year", to: "international_calendar#year", as: "international_calendar_year"
+    get "kalendaro", to: redirect("/ik")
+    get "j", to: redirect("/ik")
+    get "j/:year", to: redirect("/ik/%{year}")
+    get "/eventoj-hu", to: redirect("/ik/jaroj#eventoj-hu")
+
+    draw(:videos)
+
+    draw(:events)
+
+    # Countries and cities
+    get "/:continent", to: "events#by_continent", as: "events_by_continent"
+    get "/:continent/:country_name", controller: "events", action: "by_country", as: "events_by_country"
+    get "/:continent/:country_name/:city_name", controller: "events", action: "by_city", as: "events_by_city"
+  end
 end
