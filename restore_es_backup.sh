427c93451bcfc4a5d4c417b02191eeca325e52f0
Fernando Shayani <shayani@gmail.com>
2024-06-29 10:03:11 +0000

    chore: update restore script

--- restore_es_backup.sh
+++ restore_es_backup.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-PGPASSWORD=postgres psql -d eventaservo_devel -h localhost -U postgres -c 'drop schema public CASCADE;'
-PGPASSWORD=postgres psql -d eventaservo_devel -h localhost -U postgres -c 'create schema public;'
+DB_HOST=${DB_HOST:-localhost}
+
+PGPASSWORD=postgres psql -h "$DB_HOST" -d eventaservo_devel -U postgres -c 'drop schema public CASCADE;'
+PGPASSWORD=postgres psql -h "$DB_HOST" -d eventaservo_devel -U postgres -c 'create schema public;'
 PGPASSWORD=postgres pg_restore \
-  --host localhost \
+  --host "$DB_HOST" \
   --username postgres \
   --dbname "eventaservo_devel" \
   --role "postgres" \
