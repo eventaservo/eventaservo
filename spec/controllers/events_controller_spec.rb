77ee8e014a96a59c015c86a1f127a26154e16877
Fernando Shayani <shayani@gmail.com>
2024-11-03 22:31:12 +0000

    test: adds Selenium for remote E2E tests

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,6 +1,32 @@
 require "rails_helper"
 
 RSpec.describe EventsController, type: :request do
+  describe "GET #index" do
+    subject { get events_path }
+
+    it_behaves_like "authenticated user reuqired"
+  end
+
+  describe "GET #show" do
+    subject { get event_path(code: event.code) }
+
+    let(:event) { create(:event) }
+
+    it "returns http success" do
+      subject
+      expect(response).to have_http_status(:success)
+    end
+
+    context "when the event doesn't exist anymore" do
+      before { event.destroy }
+
+      it "redirects to the home page" do
+        subject
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+
   describe "GET #kronologio" do
     subject { get event_kronologio_path(event_code: event.code) }
 
