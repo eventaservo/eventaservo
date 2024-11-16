2cd5f2de60925deacc86f0e35185e2ca94a7f44b
Fernando Shayani <shayani@gmail.com>
2024-11-16 09:39:54 +0000

    feat(Event): :sparkles: Add to calendar links

---
+++ app/presenters/calendar/providers/google.rb
@@ -0,0 +1,22 @@
+module Calendar
+  module Providers
+    class Google < AddToCalendarPresenter
+      # @return [String] The URL to add the event to the calendar
+      def url
+        base_url = "https://www.google.com/calendar/render"
+
+        params = {
+          action: "TEMPLATE",
+          text: event.title,
+          dates: "#{event.date_start.strftime("%Y%m%dT%H%M%SZ")}/#{event.date_end.strftime("%Y%m%dT%H%M%SZ")}",
+          details: details,
+          location: location
+        }
+
+        uri = URI(base_url)
+        uri.query = URI.encode_www_form(params)
+        uri.to_s
+      end
+    end
+  end
+end
