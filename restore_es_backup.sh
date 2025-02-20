a627ed2dbbb9d1d517b1424ecafbb5ef09f50ab9
Fernando Shayani <shayani@gmail.com>
2025-02-20 21:57:40 +0000

    feat: Add role creation for eventaservo in backup restoration script

--- restore_es_backup.sh
+++ restore_es_backup.sh
@@ -4,6 +4,7 @@ DB_HOST=${DB_HOST:-localhost}
 
 PGPASSWORD=postgres psql -h "$DB_HOST" -d eventaservo_devel -U postgres -c 'drop schema public CASCADE;'
 PGPASSWORD=postgres psql -h "$DB_HOST" -d eventaservo_devel -U postgres -c 'create schema public;'
+PGPASSWORD=postgres psql -h "$DB_HOST" -d eventaservo_devel -U postgres -c 'CREATE ROLE eventaservo;'
 PGPASSWORD=postgres pg_restore \
   --host "$DB_HOST" \
   --username postgres \
