367bd4728a9c3c202eafb1ef6b1f742407795fde
Fernando Shayani <shayani@gmail.com>
2025-02-21 11:08:13 +0000

    Add format to events

--- db/migrate/20190830125930_create_eventa_enhavo_rich_text.rb
+++ db/migrate/20190830125930_create_eventa_enhavo_rich_text.rb
@@ -1,22 +1,22 @@
 class CreateEventaEnhavoRichText < ActiveRecord::Migration[6.0]
   def up
-    PaperTrail.enabled = false
-    i = Event.all.count
-    Event.all.each do |e|
-      e.enhavo = e.content
-      e.save
-      printf "#{i}     \r"
-      i -= 1
-    end
-    PaperTrail.enabled = true
+    # PaperTrail.enabled = false
+    # i = Event.all.count
+    # Event.all.each do |e|
+    #   e.enhavo = e.content
+    #   e.save
+    #   printf "#{i}     \r"
+    #   i -= 1
+    # end
+    # PaperTrail.enabled = true
   end
 
   def down
-    PaperTrail.enabled = false
-    Event.all.each do |e|
-      e.enhavo = nil
-      e.save
-    end
-    PaperTrail.enabled = true
+    # PaperTrail.enabled = false
+    # Event.all.each do |e|
+    #   e.enhavo = nil
+    #   e.save
+    # end
+    # PaperTrail.enabled = true
   end
 end
