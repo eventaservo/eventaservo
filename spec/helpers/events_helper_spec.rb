2cb2ef701459b057f66870a956e8daf054beee32
Fernando Shayani <shayani@gmail.com>
2025-02-21 10:34:32 +0000

    Rename tag Internacia to Kunveno/Evento

---
+++ spec/helpers/events_helper_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe EventsHelper, type: :helper do
+  describe "#speconomo_plurale" do
+    it "returns plural form for known tags" do
+      expect(helper.speconomo_plurale("Kunveno/Evento")).to eq("Kunvenoj/Eventoj")
+      expect(helper.speconomo_plurale("Loka")).to eq("Lokaj kunvenoj")
+      expect(helper.speconomo_plurale("Kurso")).to eq("Kursoj")
+      expect(helper.speconomo_plurale("Alia")).to eq("Aliaj")
+    end
+
+    it "returns the same tag for unknown values" do
+      expect(helper.speconomo_plurale("Unknown")).to eq("Unknown")
+    end
+  end
+end
