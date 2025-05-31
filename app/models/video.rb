c74ce61d1a1fd3867a25b00954ac078a663d85cf
Fernando Shayani <shayani@gmail.com>
2025-05-31 01:04:56 +0000

    fix(Video): Thumbnail for Youtube Shorts

--- app/models/video.rb
+++ app/models/video.rb
@@ -18,7 +18,7 @@ class Video < ApplicationRecord
   belongs_to :evento, class_name: "Event", foreign_key: "event_id"
 
   before_save :validas_ligilon
-  after_save :elshultas_bildon
+  after_save :save_thumbnail
 
   def youtube?
     url.include?("youtube.com") || url.include?("youtu.be")
@@ -27,8 +27,9 @@ class Video < ApplicationRecord
   def youtube_id
     return nil unless youtube?
 
-    regex = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/
-    url[regex, 1]
+    if url =~ %r{(youtu\.be/|youtube\.com/(watch\?v=|shorts/))([-\w]+)}
+      $3
+    end
   end
 
   def self.serchi(teksto)
@@ -51,11 +52,13 @@ class Video < ApplicationRecord
     end
   end
 
-  def elshultas_bildon
+  def save_thumbnail
     return false unless youtube?
     return false unless saved_change_to_url?
 
     file = URI.open("https://i.ytimg.com/vi/#{youtube_id}/mqdefault.jpg")
     bildo.attach(io: file, filename: "bildo.jpg", content_type: "image/jpg")
+  rescue => e
+    Sentry.capture_exception(e)
   end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -24,4 +24,34 @@ RSpec.describe "Video", type: :model do
       it { is_expected.to be_falsey }
     end
   end
+
+  describe "#youtube_id" do
+    subject { video.youtube_id }
+
+    let(:video) { build(:video, url:) }
+
+    context "when the url has youtube.com" do
+      let(:url) { "https://www.youtube.com/watch?v=123456" }
+
+      it { is_expected.to eq("123456") }
+    end
+
+    context "when the url has youtu.be" do
+      let(:url) { "https://youtu.be/123456" }
+
+      it { is_expected.to eq("123456") }
+    end
+
+    context "when the url does not have youtube.com or youtu.be" do
+      let(:url) { "https://example.com" }
+
+      it { is_expected.to be_nil }
+    end
+
+    context "when the url has a youtube short video" do
+      let(:url) { "https://www.youtube.com/shorts/123456" }
+
+      it { is_expected.to eq("123456") }
+    end
+  end
 end
