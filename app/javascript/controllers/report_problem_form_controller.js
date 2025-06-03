333eae50dc3ac0dd1a4536ba7669519a85c98ea3
Fernando Shayani <shayani@gmail.com>
2025-06-03 00:50:31 +0000

    fix: ReferenceError
    Fixes EVENTA-SERVO-VZ

--- app/javascript/controllers/report_problem_form_controller.js
+++ app/javascript/controllers/report_problem_form_controller.js
@@ -2,13 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="report-problem-form"
 export default class extends Controller {
+  static targets = ["name", "message"]
+
   connect() {
-    $('#reportProblemModal').on('shown.bs.modal', function () {
-      if ($('#name').val() === "") {
-        $('#name').focus();
-      } else {
-        $('#message').focus();
-      }
-    });
+    $(this.element).on('shown.bs.modal', this.handleModalShown.bind(this))
+  }
+
+  disconnect() {
+    $(this.element).off('shown.bs.modal', this.handleModalShown.bind(this))
+  }
+
+  handleModalShown(event) {
+    if (this.hasNameTarget && this.nameTarget.value === "") {
+      this.nameTarget.focus()
+    } else if (this.hasMessageTarget) {
+      this.messageTarget.focus()
+    }
   }
 }
