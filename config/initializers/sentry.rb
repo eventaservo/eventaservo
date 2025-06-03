c2954575e48ca562478891ca98b8eacda34948a5
Fernando Shayani <shayani@gmail.com>
2025-06-03 00:41:11 +0000

    Enable Sentry logs

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,14 +4,15 @@ Sentry.init do |config|
   config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-  # Set traces_sample_rate to 1.0 to capture 100%
+  # Set traces_sample_rate to 0.2 to capture 20%
   # of transactions for performance monitoring.
-  # We recommend adjusting this value in production.
-  config.traces_sample_rate = 0.5
+  # This provides sufficient data while keeping costs reasonable.
+  config.traces_sample_rate = 0.2
   # or
   # config.traces_sampler = lambda do |_context|
   #   true
   # end
 
   config.enabled_environments = %w[production staging]
+  config.enable_logs = true
 end
