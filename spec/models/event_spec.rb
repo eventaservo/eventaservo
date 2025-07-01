d75afafb6ab5241c6bbf64c5ac000cbd9b66805d
Fernando Shayani <shayani@gmail.com>
2025-07-01 01:53:45 +0000

    add spec for scope

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -116,6 +116,15 @@ RSpec.describe Event, type: :model do
   end
 
   describe "scopes" do
+    describe ".with_tags" do
+      it "returns events with the given tags" do
+        test_tag = create(:tag, name: "Test", group_name: "characteristic")
+        event_with_tag = create(:event, tags: [test_tag])
+        _event_without_tag = create(:event)
+        expect(Event.with_tags([test_tag.id])).to eq([event_with_tag])
+      end
+    end
+
     describe ".without_tag" do
       it "returns events without the given tag" do
         _event_with_tag = create(:event, tags: [create(:tag, name: "Test", group_name: "characteristic")])
