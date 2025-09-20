7a186939f9fa8d69ab6d3d1a1220e0ee0f680a34
Fernando Shayani <shayani@gmail.com>
2025-09-20 16:08:46 -0300

    feat: enhance event duplication by preserving tags and characteristics

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -100,6 +100,7 @@ Rails.application.configure do
   config.hosts << "fr.eventaservo.local:3000"
   config.hosts << "nl.eventaservo.local:3000"
   config.hosts << "pt_BR.eventaservo.local:3000"
+  config.hosts << "eventaservo.org.localhost"
 
   config.active_job.default_url_options = {host: "localhost", port: 3000, protocol: :http}
   Rails.application.routes.default_url_options[:host] = "localhost"
