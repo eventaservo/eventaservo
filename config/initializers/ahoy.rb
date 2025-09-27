f580eb45a271742f3381efd26fcd9d3a55491cef
Fernando Shayani <shayani@gmail.com>
2025-09-27 16:00:29 -0300

    feat(Ahoy): enable bot tracking in Ahoy configuration

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -8,6 +8,9 @@ end
 # set to true for JavaScript tracking
 Ahoy.api = false
 
+# Track bots
+Ahoy.track_bots = true
+
 # set to true for geocoding (and add the geocoder gem to your Gemfile)
 # we recommend configuring local geocoding as well
 # see https://github.com/ankane/ahoy#geocoding
