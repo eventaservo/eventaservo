696755a1916ed38f6aa2c6d2525fb26e81e0c091
Fernando Shayani <shayani@gmail.com>
2026-01-17 05:12:42 -0300

    refactor: make LogFactory build and create methods public

--- app/factories/log_factory.rb
+++ app/factories/log_factory.rb
@@ -23,28 +23,34 @@
 #   log.text #=> nil
 #
 class LogFactory
-  class << self
-    # Builds a new Log instance without saving to database.
-    #
-    # @param attributes [Hash] attributes for the Log
-    # @option attributes [String, nil] :text the log text
-    # @option attributes [User, nil] :user the user who created the log
-    # @option attributes [Object, nil] :loggable polymorphic associated object
-    # @return [Log] unsaved Log instance
-    def build(attributes = {})
-      Log.new(attributes)
-    end
+  attr_reader :loggable, :user, :text
 
-    # Creates and saves a new Log instance.
-    #
-    # @param attributes [Hash] attributes for the Log
-    # @option attributes [String, nil] :text the log text
-    # @option attributes [User, nil] :user the user who created the log
-    # @option attributes [Object, nil] :loggable polymorphic associated object
-    # @return [Log] persisted Log instance
-    # @raise [ActiveRecord::RecordInvalid] if validation fails
-    def create(attributes = {})
-      Log.create!(attributes)
-    end
+  def initialize(loggable: nil, user: nil, text: nil)
+    @loggable = loggable
+    @user = user
+    @text = text
+  end
+
+  def self.build(loggable: nil, user: nil, text: nil)
+    new(loggable:, user:, text:).build
+  end
+
+  def self.create(loggable: nil, user: nil, text: nil)
+    new(loggable:, user:, text:).create
+  end
+
+  # Builds a new Log instance without saving to database.
+  #
+  # @return [Log] unsaved Log instance
+  def build
+    Log.new(loggable:, user:, text:)
+  end
+
+  # Creates and saves a new Log instance.
+  #
+  # @return [Log] persisted Log instance
+  # @raise [ActiveRecord::RecordInvalid] if validation fails
+  def create
+    Log.create!(loggable:, user:, text:)
   end
 end
