63b99bff920b5ae0d2cd029734460455aba54ef7
Fernando Shayani <shayani@gmail.com>
2026-01-16 22:46:48 -0300

    docs: add more YARD examples to LogFactory

--- app/factories/log_factory.rb
+++ app/factories/log_factory.rb
@@ -3,10 +3,24 @@
 # Factory for creating Log instances.
 #
 # @example Build an unsaved Log
-#   LogFactory.build(text: "My log", user: user)
+#   log = LogFactory.build(text: "My log", user: user)
+#   log.save
 #
 # @example Create a persisted Log
-#   LogFactory.create(text: "My log", user: user, loggable: event)
+#   log = LogFactory.create(text: "My log", user: user, loggable: event)
+#
+# @example Create a log with polymorphic association
+#   organization = create(:organization)
+#   log = LogFactory.create(text: "Organization updated", loggable: organization)
+#   log.loggable #=> organization
+#
+# @example Create a log without user (uses system account)
+#   log = LogFactory.create(text: "System event")
+#   log.user #=> User.system_account
+#
+# @example Create a log without text
+#   log = LogFactory.create(user: user)
+#   log.text #=> nil
 #
 class LogFactory
   class << self
