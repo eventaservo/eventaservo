c2dacda79bd64bf828213b74bdd68b10be38f872
Fernando Shayani <shayani@gmail.com>
2026-01-17 09:41:09 +0000

    chore: update Gemfile and seeds.rb for FactoryBot and Faker integration

--- app/jobs/create_sample_events_job.rb
+++ app/jobs/create_sample_events_job.rb
@@ -3,11 +3,12 @@
 class CreateSampleEventsJob < ApplicationJob
   def perform(user_id)
     return false if Rails.env.production?
+    return false unless defined?(FactoryBot)
 
     user = User.find_by(id: user_id)
     return false unless user&.admin?
 
-    FactoryBot.create_list(:event, 20, :future, user: user)
-    FactoryBot.create_list(:event, 5, :past, user: user)
+    FactoryBot.create_list(:event, 20, :future, user:)
+    FactoryBot.create_list(:event, 5, :past, user:)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ puts "== Running seeds.rb file"
 puts "- Loading Fixtures"
 Rake.application["db:fixtures:load"].invoke
 
-# Kreas la administranton
+# Create admin user
 User.destroy_all
 User.create(
   email: "admin@eventaservo.org",
@@ -27,41 +27,48 @@ User.create(
 )
 
 puts "Create System Account"
-FactoryBot.create(:user, name: "EventaServo Sistemo", email: "kontakto@eventaservo.org", system_account: true)
+User.create!(
+  name: "EventaServo Sistemo",
+  email: "kontakto@eventaservo.org",
+  system_account: true,
+  password: SecureRandom.hex(16),
+  confirmed_at: DateTime.now
+)
 
 puts "- Creating organizations"
-FactoryBot.create(:organization, :uea)
-FactoryBot.create(:organization, :tejo)
-3.times { FactoryBot.create(:organization) }
+Organization.create!(
+  name: "Universala Esperanto Asocio",
+  short_name: "UEA",
+  city: "Rotterdam",
+  country: Country.find_by(code: "nl"),
+  email: "uea@uea.org",
+  major: true
+)
 
-# Common users
-puts "- Creating users and events"
-2.times do
-  user = FactoryBot.create(:user)
-  puts "  - User #{user.name}"
-  4.times do
-    puts "  \\_ creating future event"
-    FactoryBot.create(:event, user:)
-  end
-  3.times do
-    puts "  \\_ creating past event"
-    FactoryBot.create(:event, :past, user:)
-  end
-  3.times do
-    puts "  \\_ creating future online event"
-    FactoryBot.create(:event, :online, user:)
-  end
-end
+Organization.create!(
+  name: "Tutmonda Esperanta Junulara Organizo",
+  short_name: "TEJO",
+  city: "Rotterdam",
+  country: Country.find_by(code: "nl"),
+  email: "info@tejo.org",
+  major: true
+)
 
-# International events
-puts "- Creating international calendar events"
-3.times do
-  FactoryBot.create(
-    :event,
-    :international_calendar,
-    user: User.all.sample,
-    date_start: DateTime.now + rand(1..3).months
-  )
+# Fake data only in development (when FactoryBot is available)
+if Rails.env.development? && defined?(FactoryBot)
+  puts "- Creating sample users and events (development only)"
+  2.times do
+    user = FactoryBot.create(:user)
+    puts "  - User #{user.name}"
+    4.times { FactoryBot.create(:event, user:) }
+    3.times { FactoryBot.create(:event, :past, user:) }
+    3.times { FactoryBot.create(:event, :online, user:) }
+  end
+
+  puts "- Creating international calendar events"
+  3.times do
+    FactoryBot.create(:event, :international_calendar, user: User.all.sample, date_start: DateTime.now + rand(1..3).months)
+  end
 end
 
 puts "== FINISHED"
