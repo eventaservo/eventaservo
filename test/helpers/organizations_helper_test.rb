8a88cea02e568b5216f5da0c62043e429e07c186
Fernando Shayani <shayani@gmail.com>
2026-03-07 01:41:49 +0000

    chore: implement global link reset for Bootstrap 5 compatibility

--- test/helpers/organizations_helper_test.rb
+++ test/helpers/organizations_helper_test.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 class OrganizationsHelperTest < ActionView::TestCase
   include OrganizationsHelper
 
-  test 'display_event_tags returns correct bootstrap 5 classes' do
-    country = Country.first || Country.create!(name: 'Test', code: 'TS', continent: 'Europe')
-    user = User.first || User.create!(email: "test#{rand(1000)}@example.com", password: 'password', username: "testuser#{rand(1000)}")
-    
+  test "display_event_tags returns correct bootstrap 5 classes" do
+    country = Country.first || Country.create!(name: "Test", code: "TS", continent: "Europe")
+    user = User.first || User.create!(email: "test#{rand(1000)}@example.com", password: "password", username: "testuser#{rand(1000)}")
+
     event = Event.create!(
-      title: 'Test Event', 
-      description: 'Test Description',
-      city: 'Test City',
+      title: "Test Event",
+      description: "Test Description",
+      city: "Test City",
       country: country,
       user: user,
       date_start: Time.zone.now,
       date_end: Time.zone.now + 1.hour,
       code: "TEST#{rand(1000)}",
-      site: 'https://eventaservo.org'
+      site: "https://eventaservo.org"
     )
-    
-    category_tag = Tag.find_or_create_by!(name: 'Kategorio', group_name: 'category')
-    characteristic_tag = Tag.find_or_create_by!(name: 'Karakterizo', group_name: 'characteristic')
-    
+
+    category_tag = Tag.find_or_create_by!(name: "Kategorio", group_name: "category")
+    characteristic_tag = Tag.find_or_create_by!(name: "Karakterizo", group_name: "characteristic")
+
     event.tags << category_tag
     event.tags << characteristic_tag
 
