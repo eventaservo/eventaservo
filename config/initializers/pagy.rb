5240ef1d488f2c3abf560fde8491284f638c2289
Fernando Shayani <shayani@gmail.com>
2026-04-08 08:15:11 +0000

    style: fix standardrb violations in pagy initializer

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,14 +1,10 @@
 # frozen_string_literal: true
 
 # Pagy DEFAULT Variables
-Pagy::DEFAULT.merge!(
-  items: 15,
-  page_param: :pagho
-)
+Pagy::DEFAULT[:items] = 15
+Pagy::DEFAULT[:page_param] = :pagho
 
 # Extras
 require "pagy/extras/array"
 require "pagy/extras/bootstrap"
 require "pagy/extras/trim"
-
-# Pagy::DEFAULT.freeze # Not strictly needed if it's already frozen or not required by 9.x
