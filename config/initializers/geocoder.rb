c43318657039169958928b6862b030a4c41f0d61
Fernando Shayani <shayani@gmail.com>
2026-05-10 19:13:58 -0300

    feat: report geocoding failures to Sentry and logs (#1191)
    * fix: report geocoding failures to Sentry and logs
    
    * docs: add YARD to geocode and increase test coverage
    
    * fix: handle geocoding exceptions in Importilo service
    
    * chore: fix linting issues in geocoding test

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -23,7 +23,7 @@ Geocoder.configure(
   # Exceptions that should not be rescued by default
   # (if you want to implement custom error handling);
   # supports SocketError and Timeout::Error
-  # always_raise: [],
+  always_raise: :all,
 
   # Calculation options
   units: :km,                 # :km for kilometers or :mi for miles
