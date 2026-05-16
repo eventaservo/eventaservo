9b87c1c283a0c244f11ad2af387de5231bd61bc5
Fernando Shayani <shayani@gmail.com>
2026-05-16 08:50:00 -0300

    refactor: replace InternationalEventBadgeComponent with a partial
    Action Point 1 of removing view_component gem (issue #1203).

--- test/components/previews/international_event_badge_component_preview.rb
+++
@@ -1,8 +0,0 @@
-class InternationalEventBadgeComponentPreview < ViewComponent::Preview
-  layout "component_preview"
-
-  def default
-    event = Event.new(international_calendar: true)
-    render(InternationalEventBadgeComponent.new(event: event))
-  end
-end
