337853bb222dbb77bc876f810ab61f0331a6761e
Fernando <fer@email.com>
2026-06-20 05:55:08 -0300

    Revert unrelated video_controller.rb change, keep only nokogiri update

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -11,8 +11,9 @@ class VideoController < ApplicationController
 
   def new
     @evento = Event.by_link(params[:event_code])
-    if !user_can_edit_event?(user: current_user, event: @evento)
+    unless user_can_edit_event?(user: current_user, event: @evento)
       redirect_to root_url, flash: {error: "Vi ne rajtas"}
+      return
     end
   end
 
